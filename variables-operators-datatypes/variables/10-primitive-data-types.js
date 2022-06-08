86b993147e25231ae0ff98398793967c24ca76a5
EFTEKHER <eftekherali2000@gmail.com>
2022-06-08 16:02:53 +0600

    fight

--- variables-operators-datatypes/variables/10-primitive-data-types.js
+++ variables-operators-datatypes/variables/10-primitive-data-types.js
@@ -3,8 +3,16 @@
 let a=9;
 let b=a;
 //deals with value
-
+//different address
 a=45;
 console.log(b); //don't effect value in changing
 //Reference types
 //array,object
+//same address
+
+let numbers=[1,2,3];
+let newnumbers=numbers;
+console.log(newnumbers);
+numbers[1]=500;
+console.log(numbers);
+console.log(newnumbers);
